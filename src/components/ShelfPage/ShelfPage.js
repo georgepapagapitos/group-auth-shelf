ebfd7b12cc3bd589dcd8e7b4bd792c4b9a2f2000
Vada Karlen <vada.karlen@gmail.com>
2021-03-11 16:41:40 -0600

    Added key to map of items

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -3,36 +3,36 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 function ShelfPage() {
-
   const [items, setItems] = useState([]);
 
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
     getItems();
   }, []);
 
   const getItems = () => {
-    axios.get('/api/shelf/')
-    .then((res) => {
-      setItems(res.data)
-    })
-    .catch((err) => {
-      console.log('Error in GET', err)
-    })
-  }
+    axios
+      .get('/api/shelf/')
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((err) => {
+        console.log('Error in GET', err);
+      });
+  };
 
   return (
     <div className="container">
       <h2>Shelf</h2>
       <div className="item-container">
-        {items.map(item => {
+        {items.map((item) => {
           return (
-            <div>
+            <div key={item.id}>
               <img src={item.image_url} alt={item.description} />
               <p>{item.description}</p>
             </div>
-          )
+          );
         })}
       </div>
     </div>
